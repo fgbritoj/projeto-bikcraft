15a9d514b506f36130c383f761fd2ee0ae8d7d61
Brito Junior <113487328+fgbritoj@users.noreply.github.com>
2023-11-20 01:20:14 -0300

    menu ativo

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,11 @@
 // Header - Marcar página selecionada 
+const links = document.querySelectorAll('.header-menu a')
 
+links.forEach(link => {
+    if(location.href.includes(link.href)) {
+        link.classList.add('ativo')
+    }
+})
 
 // Seguros - Perguntas (Função para mostrar Respostas ao clique)
 const perguntas = document.querySelectorAll('.pergunta-item')
@@ -7,7 +13,7 @@ const perguntas = document.querySelectorAll('.pergunta-item')
 perguntas.forEach(pergunta => {
     pergunta.addEventListener('click', () => {
         pergunta.nextElementSibling.classList.toggle('mostrar')
-            
+
         const seta = pergunta.querySelector('img')
         seta.classList.toggle('rotacionar')
     })
